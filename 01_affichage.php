19cc81a0027b9be27ecbd1e08f375ae6b3cc5110
moukengue aline <aline.moukengue80@gmail.com>
2023-03-01 10:34:03 +0100

    first commit

---
+++ 01_affichage.php
@@ -0,0 +1,12 @@
+<?php
+/*echo "Bonjour le monde"; 
+echo 'Bonjour le monde'; 
+echo "Bonjour a vous ce matin";
+echo 123;
+echo'aline';*/
+//print ('Moukengue');
+//print_r ([1,2,3,5]);
+//var_dump([1,8,3,14]);
+//var_export(123);
+//var_export('salut les pros')
+?>
---
+++ 02_variable.php
@@ -0,0 +1,7 @@
+<?
+//arrys /tableau
+$nombres = [1,25,14,180,50];
+print_r($nombres[3]);
+
+
+   
